762494ccffa1ba64221964773eecd1148d1ba573
UlrichBR <38494475+UlrichBR@users.noreply.github.com>
2024-09-29 20:33:17 -0300

    Update EnumConfigurationRepresenter.java

--- src/main/java/me/ulrich/econfig/yaml/EnumConfigurationRepresenter.java
+++ src/main/java/me/ulrich/econfig/yaml/EnumConfigurationRepresenter.java
@@ -1,18 +1,25 @@
 package me.ulrich.econfig.yaml;
 
+import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.nodes.Node;
 import org.yaml.snakeyaml.representer.Representer;
 
 import me.ulrich.econfig.yaml.file.interfaces.ConfigurationSection;
 
 public class EnumConfigurationRepresenter extends Representer {
+
     public EnumConfigurationRepresenter() {
+        // Chame o construtor da classe Representer que aceita DumperOptions
+        super(new DumperOptions());
+        
+        // Adicione o representer personalizado para ConfigurationSection
         this.multiRepresenters.put(ConfigurationSection.class, new RepresentConfigurationSection());
     }
 
     private class RepresentConfigurationSection extends RepresentMap {
         @Override
         public Node representData(Object data) {
+            // Converta ConfigurationSection para um mapa e represente seus dados
             return super.representData(((ConfigurationSection) data).getValues(false));
         }
     }
